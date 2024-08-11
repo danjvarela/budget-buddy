cf9b2983e917589596b41ccc38d43995475f1f1d
Dan Varela <vareladanmarj@gmail.com>
2024-08-11 22:15:00 +0800

    fix: rewrite url state manager
    - debounce update to search params
    - get initial value from search params

--- src/lib/url-state-manager.ts
+++ src/lib/url-state-manager.ts
@@ -1,54 +1,53 @@
-import { useCallback, useEffect, useMemo, useState } from "react"
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { debounce } from "lodash"
 
 type UrlStateValue = string | number
 
 export function useUrlState<T extends UrlStateValue>(
   key: string,
   initialValue: T
-): [T, (value: T | ((previousValue: T) => T)) => void] {
-  const [initialized, setInitialized] = useState(false)
+): [T, Dispatch<SetStateAction<T>>] {
   const searchParams = useSearchParams()
   const router = useRouter()
   const pathname = usePathname()
 
-  const value = useMemo(() => {
-    if (!initialized) return initialValue
-
-    const v = searchParams.get(key)
+  const resolveSearchParamValue = useCallback(() => {
+    const s = searchParams.get(key)
+    if (s === null) return initialValue
 
     if (typeof initialValue === "number") {
-      return parseFloat(v as string) as T
+      const parsed = parseFloat(s)
+      if (isNaN(parsed)) return initialValue
+      return parsed as T
+    } else {
+      return s as T
     }
+  }, [searchParams])
 
-    return v as T
-  }, [initialValue, key, searchParams, initialized])
+  const [value, setValue] = useState<T>(resolveSearchParamValue)
 
-  const setValue = useCallback(
-    (newValue: T | ((prevValue: T) => T)) => {
-      const resolvedNewValue =
-        typeof newValue === "function" ? newValue(value) : newValue
-
-      // don't do anything if value is not changed
-      if (resolvedNewValue === value) return
-
-      const s = new URLSearchParams(searchParams.toString())
-
-      s.set(key, resolvedNewValue.toString())
-
-      router.replace(`${pathname}?${s.toString()}`)
+  const updateSearchParams = useCallback(
+    (value: T) => {
+      const mutableSearchParams = new URLSearchParams(searchParams.toString())
+      mutableSearchParams.set(key, value.toString())
+      router.replace(`${pathname}?${mutableSearchParams.toString()}`, {
+        scroll: false,
+      })
     },
-    [searchParams, key, pathname, router, value]
+    [searchParams, key, router, pathname]
   )
 
   useEffect(() => {
-    if (!initialized) {
-      const s = new URLSearchParams(searchParams.toString())
-      s.set(key, initialValue.toString())
-      setInitialized(true)
-      router.replace(`${pathname}?${s.toString()}`)
-    }
-  }, [router, searchParams, pathname, key, initialValue])
+    const debouncedUpdatedSearchParams = debounce(updateSearchParams, 1000)
+    debouncedUpdatedSearchParams(value)
+  }, [value, updateSearchParams])
 
   return [value, setValue]
 }
