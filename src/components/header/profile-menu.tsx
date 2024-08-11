72e2f8b0f1a509fc8db1dd28c2c6d39f20a28869
Dan Varela <vareladanmarj@gmail.com>
2024-08-11 22:15:03 +0800

    feat: add a backdrop when logging out

--- src/components/header/profile-menu.tsx
+++ src/components/header/profile-menu.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { logoutAction } from "@/actions/auth"
 import { LogOut, User } from "lucide-react"
+import { useAction } from "next-safe-action/hooks"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -14,32 +17,42 @@ import { useSession } from "@/components/auth-provider"
 export function ProfileMenu() {
   const session = useSession()
   const router = useRouter()
+  const { executeAsync: logout, isExecuting } = useAction(logoutAction)
 
   async function handleLogOut() {
-    await logoutAction()
-    router.push("/")
+    const res = await logout()
+    if (res?.data?.success) {
+      router.push("/")
+    }
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="max-w-[200px]">
-          <span className="w-full truncate">{session?.email}</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="min-w-[150px]">
-        <DropdownMenuItem asChild>
-          <Link href="/profile">
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-          </Link>
-        </DropdownMenuItem>
+    <>
+      {isExecuting && (
+        <div className="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-muted/50 backdrop-blur">
+          Signing out...
+        </div>
+      )}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="max-w-[200px]">
+            <span className="w-full truncate">{session?.email}</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="min-w-[150px]">
+          <DropdownMenuItem asChild>
+            <Link href="/profile">
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </Link>
+          </DropdownMenuItem>
 
-        <DropdownMenuItem onClick={handleLogOut}>
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sign out</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem onClick={handleLogOut}>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sign out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   )
 }
